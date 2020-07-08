36873fd8ed2f0504ff673052297a8eaa76dc84ca
MagmMe <marcin@magm.me>
2020-07-08 06:46:11 +0200

    Initial Commit

---
+++ StoryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryApp
+//
+//  Created by Marcin Magiera on 08/07/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
